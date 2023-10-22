55eb6968e60f00974e0639a915dbe87391e4483d
abrahamshepher <abrahamshepher@gmail.com>
2023-10-22 14:51:25 +0500

    replaced cards

--- src/components/ModelWorking.js
+++ src/components/ModelWorking.js
@@ -1,77 +1,133 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import city1 from "../assets/city1.png";
-import city2 from "../assets/city2.png";
-import city3 from "../assets/city3.png";
-import planet1 from "../assets/planet1.png";
-import planet2 from "../assets/planet2.png";
-import SpaceOrange1 from "../assets/SpaceOrange1.jpeg";
-
+import Image from "../constants/Image";
 function ModelWorking() {
-  const [expandedIndex, setExpandedIndex] = useState(null);
-
-  const handleCardClick = (index) => {
-    setExpandedIndex(index === expandedIndex ? -1 : index);
-  };
-
-  const cardVariants = {
-    expanded: {
-      width: "400px",
+  const CardsData = [
+    {
+      image: (
+        <Image
+          alt="Image 1"
+          width={40}
+          height={40}
+          dataNimg={1}
+          className="w-full h-full object-contain object-center"
+          src="/images/4.svg"
+        />
+      ),
+      title: "Easy Searching",
+      text: "Effortlessly find people using fields like name, department, and job title with OrgFlow's powerful search features.",
     },
-    collapsed: {
-      width: "200px",
+    {
+      image: (
+        <Image
+          alt="Image 2"
+          width={40}
+          height={40}
+          dataNimg={1}
+          className="w-full h-full object-contain object-center"
+          src="/images/2.svg"
+        />
+      ),
+
+      title: "Microsoft's Card",
+      text: "Connect, collaborate, and gather information easily with Microsoft's User Card integrated with OrgFlow.",
     },
-  };
+    {
+      image: (
+        <Image
+          alt="Image 3"
+          width={40}
+          height={40}
+          dataNimg={1}
+          className="w-full h-full object-contain object-center"
+          src="/images/3.svg"
+        />
+      ),
 
-  const cardImages = [city1, city2, city3, planet1, planet2, SpaceOrange1];
+      title: "Seamless Charts",
+      text: "Create, visualize, and export organizational charts as image or Excel files with OrgFlow's versatile capabilities.",
+    },
 
-  const cardDescriptions = [
-    "This is a description, write whatever you need here, this is just text for a test",
-    "This is a description, write whatever you need here, this is just text for a test",
-    "This is a description, write whatever you need here, this is just text for a test",
-    "This is a description, write whatever you need here, this is just text for a test",
-    "This is a description, write whatever you need here, this is just text for a test",
+    {
+      image: (
+        <Image
+          alt="Image 4"
+          width={40}
+          height={40}
+          dataNimg={1}
+          className="w-full h-full object-contain object-center"
+          src="/images/1.svg"
+        />
+      ),
+      title: "Highly Customizable",
+      text: "Tailor your organizational charts to perfection with OrgFlow's robust customization options. ",
+    },
+
+    {
+      image: (
+        <Image
+          alt="Image 5"
+          width={40}
+          height={40}
+          dataNimg={1}
+          className="w-full h-full object-contain object-center"
+          src="/images/5.svg"
+        />
+      ),
+
+      title: "Team Visualization",
+      text: "Create, visualize, and export organizational charts as image or Excel files with OrgFlow's versatile capabilities.",
+    },
+
+    {
+      image: (
+        <Image
+          alt="Image 6"
+          width={40}
+          height={40}
+          dataNimg={1}
+          className="w-full h-full object-contain object-center"
+          src="/images/6.svg"
+        />
+      ),
+
+      title: "Team Collaboration",
+      text: "Create, visualize, and export organizational charts as image or Excel files with OrgFlow's versatile capabilities.",
+    },
   ];
   return (
-    <section className="relative py-16 pb-[300px] bg-slate-200  " id="model">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <h1 className="text-3xl font-extrabold text-black">
-          Featured Projects
-        </h1>
-        <p className="mt-4 text-xl text-gray-600">Check out our latest works</p>
-      </div>
-      <div className="mt-12 flex flex-col md:flex-row justify-center items-center gap-10">
-        {[0, 1, 2, 3, 4, 5].map((index) => (
-          <motion.div
-            key={index}
-            className={`card cursor-pointer h-[500px]  bg-cover bg-center rounded-[20px]  ${
-              index === expandedIndex ? "expanded" : ""
-            }`}
-            variants={cardVariants}
-            initial="expanded"
-            animate={index === expandedIndex ? "expanded" : "collapsed"}
-            transition={{ duration: 0.5 }}
-            onClick={() => handleCardClick(index)}
-            style={{
-              backgroundImage: `url(${cardImages[index]})`,
-            }}
-          >
-            <div className="card-content h-full flex flex-col justify-end">
-              <div className="card-footer rounded-b-[20px] bg-gray-800 bg-opacity-75 min-h-[100px]  flex flex-col items-center justify-center">
-                <h2 className="text-xl font-semibold text-white text-center">
-                  Card {index + 1}
-                </h2>
-                {index === expandedIndex && (
-                  <p className="mt-2 text-gray-300 text-center">
-                    {cardDescriptions[index]}{" "}
-                  </p>
-                )}
+    <div className="relative  p-[8%] z-10 bg-gray-100" id="model">
+      <h1 className=" text-5xl text-black text-center max-[500px]:text-2xl max-[500px]:w-full">
+        Why are we the Best?
+      </h1>
+
+      {/* Cards */}
+      <div className="grid grid-cols-3 max-[900px]:grid-cols-2 max-[550px]:grid-cols-1 gap-10 m-auto my-10">
+        {CardsData.map((element, index) => {
+          return (
+            <div
+              key={index}
+              className="p-[10%] 	bg-white shadow-md rounded-md p-10 group"
+            >
+              {/* Images */}
+              <div className=" w-[20%] max-[900px]:m-auto max-[900px]:mb-10 mb-8 items-center  text-5xl  group-hover:scale-110 transition-transform ">
+                {element?.image}
+              </div>
+              {/* Text */}
+              <div className="relative max-[900px]:text-center ">
+                <h6 className="absolute -top-[22px] w-full font-bold text-sm">
+                  {element.title}
+                </h6>
+
+                <p className="bottom-0 text-body pt-2 w-full text-sm">
+                  {element.text}
+                </p>
               </div>
             </div>
-          </motion.div>
-        ))}
+          );
+        })}
       </div>
-    </section>
+    </div>
   );
 }
 
---
+++ src/constants/Image.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Images(props) {
+  return <img alt={props.alt} className={props.className} src={props.src} />;
+}
+
+export default Images;
